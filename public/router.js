ceb908f20b3990abb10251ef9a72a4215ae0e0e2
YuiOmata <yuuri.o.drm@gmail.com>
2016-05-29 14:14:10 +0900

    ガイド歌詞読む方も作った

--- public/router.js
+++ public/router.js
@@ -4,11 +4,13 @@ var router = new VueRouter()
 
 //var URL_BASE = 'https://yuitaso-ok.herokuapp.com';
 var song_name = [ 'kinigayo', 'hamamizuki', 'natuiro'];
-var create_url = function(song_id){
-console.log(song_id);
+var create_url_roma = function(song_id){
   return 'public/data/' + song_name[song_id] + '_r.txt';
 };
 
+var create_url_kana = function(song_id){
+  return 'public/data/' + song_name[song_id] + '_k.txt';
+};
 
 Vue.component("links", {
   template: "#links",
@@ -39,27 +41,39 @@ router.map({
         template: '#game',
         data: function(){
           return{
-              allKashi: 'init kashi',//歌詞全体
-              phrases: ["init1", "init2"]//行ごとに分割した配列
+              allKashi: "",//歌詞全体
+              allGuid: "",//日本語歌詞全体
+              phrases: ['init1', 'init2'],//行ごとに分割した配列
+              guitPhrases: []
           };
         },
         created: function () {
-          //set_polling(this.loadText);
-          this.loadText();
+          this.loadAllKashi();
         },
         methods: {
-          loadText: function() {
+          loadAllKashi: function() {
             var self = this;
-            self.allKashi = "new data";
-            $.get(create_url(self.$route.params.song_id), function(data){
+            $.get(create_url_roma(self.$route.params.song_id), function(data){
               self.allKashi = data;
               //self.phrases = allKashi.split(/\r\n|\r|\n/);
+              //console.log(phrases);
               console.log('readed');
             }).fail(function(){
               console.log('fail');
-              // alert("Sorry cant load file: click to go select");
+              // alert("Sorry cant load file: click to go select page");
               // router.go('/sands');
             });
+            $.get(create_url_kana(self.$route.params.song_id), function(data){
+              self.allGuid = data;
+              //self.phrases = allKashi.split(/\r\n|\r|\n/);
+              //console.log(phrases);
+              console.log('readed');
+            }).fail(function(){
+              console.log('fail');
+              // alert("Sorry cant load file: click to go select page");
+              // router.go('/sands');
+            });
+
           }
         }
       })
