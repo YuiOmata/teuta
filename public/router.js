26a0a3a1ebef79e875590444ebc186ff900a2b0b
YuiOmata <yuuri.o.drm@gmail.com>
2016-05-29 13:10:00 +0900

    歌詞を読み込めるようにした

--- public/router.js
+++ public/router.js
@@ -2,7 +2,12 @@
 var App = Vue.extend({})
 var router = new VueRouter()
 
-var URL_BASE = 'https://yuitaso-ok.herokuapp.com';
+//var URL_BASE = 'https://yuitaso-ok.herokuapp.com';
+var song_name = [ 'kinigayo', 'hamamizuki', 'natuiro'];
+var create_url = function(song_id){
+  return 'public/data/' + song_name[dong_id] + '_r.txt';
+}
+
 
 Vue.component("links", {
   template: "#links",
@@ -31,20 +36,29 @@ router.map({
       auth: true,
       component: Vue.extend({
         template: '#game',
-        // data: function () {
-        //   return {
-        //     textData: ''
-        //   };
-        // },
+        data: function(){
+          return{
+              allKashi: 'init kashi',//歌詞全体
+              phrases: ["init1", "init2"]//行ごとに分割した配列
+          };
+        },
         created: function () {
           //set_polling(this.loadText);
           this.loadText();
         },
         methods: {
           loadText: function() {
-            $.get("data/kashi_roma.txt", function(data){
-              $("#read_text").text(data);
-            })
+            var self = this;
+            self.allKashi = "new data";
+            $.get("public/data/kinigayo_r.txt", function(data){
+              self.allKashi = data;
+              //self.phrases = allKashi.split(/\r\n|\r|\n/);
+              console.log('readed');
+            }).fail(function(){
+              console.log('fail');
+              // alert("Sorry cant load file: click to go select");
+              // router.go('/sands');
+            });
           }
         }
       })
