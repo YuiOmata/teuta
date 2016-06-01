e88bb9d5a0674288775d3cfaa4ea22ffdea25257
Yui Omata <yui@Yui-no-MacBook-Pro.local>
2016-06-01 23:06:55 +0900

    hanamizuki実装

--- src/main/webapp/public/router.js
+++ src/main/webapp/public/router.js
@@ -1,7 +1,7 @@
 var App = Vue.extend({})
 var router = new VueRouter()
 
-var song_name = [ 'kinigayo', 'hanamizuki', 'natsuiro'];
+var song_name = [ 'kimigayo', 'hanamizuki', 'natsuiroegao'];
 var create_url_roma = function(song_id){
   return 'public/data/text/' + song_name[song_id] + '_r.txt';
 };
