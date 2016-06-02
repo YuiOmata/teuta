02226e442554a34901e7c0acc65db69fe8c157e7
Yui Omata <yui@Yui-no-MacBook-Pro.local>
2016-06-02 21:04:07 +0900

    css追加したよ

--- public/router.js
+++ public/router.js
@@ -3,131 +3,141 @@ var router = new VueRouter()
 
 var song_name = [ 'kimigayo', 'hanamizuki', 'natsuiroegao'];
 var create_url_roma = function(song_id){
-  return 'public/data/' + song_name[song_id] + '_r.txt';
+  return 'public/data/text/' + song_name[song_id] + '_r.txt';
 };
 var create_url_kana = function(song_id){
-  return 'public/data/' + song_name[song_id] + '_k.txt';
+  return 'public/data/text/' + song_name[song_id] + '_k.txt';
 };
 
-
 router.map({
-    '/': {
-      component: Vue.extend({
-        created: function () {
-          router.go('/top');
+  '/': {
+    component: Vue.extend({
+      created: function () {
+        router.go('/top');
+      }
+    })
+  },
+  '/top': {
+    component:  Vue.extend({
+      template: '#top'
+    })
+  },
+  '/songs': {
+    component:  Vue.extend({
+      template: '#songs',
+      data: function(){
+        return{
+          comment0: '',
+          comment1: '',
+          comment2: ''
+        };
+      }
+    })
+  },
+  '/startGame/:song_id':{
+    component:  Vue.extend({
+      template: '#startGame'
+    })
+  },
+  '/game/:song_id': {
+    auth: true,
+    component: Vue.extend({
+      template: '#game',
+      data: function(){
+        return{
+            allKashi: "",//歌詞全体
+            allGuid: "",//日本語歌詞全体
+            phrases: [],//行ごとに分割した配列
+            guidPhrases: [],
+            phrasesLength: 0,//
+            stillStart: false,//以下ふらぐ共
+            isPlaying: true,
+            isClear: false,
+            failInput: false,
+            onfocus: 0,//処理中の行
+            inputString: '',
+            inputChar: '',
+            combo: 0,
+            maxCombo: 0,
+            fullCombo: 0
+        };
+      },
+      created: function () {
+        this.loadAllKashi();
+      },
+      computed: {
+        inputStringCurrent: function(index){
+          return this.inputString;
+        },
+        getCombo: function(){
+          return this.combo;
+        },
+        getMaxCombo: function(){
+          return this.maxCombo;
         }
-      })
-    },
-    '/top': {
-      component:  Vue.extend({
-        template: '#top'
-      })
-    },
-    '/songs': {
-      component:  Vue.extend({
-        template: '#songs'
-      })
-    },
-    '/initPage/:song_id':{
-      component:  Vue.extend({
-        template: '#initPage'
-      })
-    },
-    '/game/:song_id': {
-      auth: true,
-      component: Vue.extend({
-        template: '#game',
-        data: function(){
-          return{
-              allKashi: "",//歌詞全体
-              allGuid: "",//日本語歌詞全体
-              phrases: [],//行ごとに分割した配列
-              guidPhrases: [],
-              phrasesLength: 0,//
-              stillStart: false,//以下ふらぐ共
-              isPlaying: true,
-              isClear: false,
-              failInput: false,
-              onfocus: 0,//処理中の行
-              inputString: '',
-              inputChar: '',
-              combo: 0,
-              maxCombo: 0
-          };
+      },
+      methods: {
+        loadAllKashi: function() {
+          var self = this;
+          $.get(create_url_roma(self.$route.params.song_id), function(data){
+            self.allKashi = data;
+            self.phrases = self.allKashi.split(/\r\n|\r|\n/);
+            self.phrases.pop();
+            self.phrasesLength = self.phrases.length;
+            self.fullCombo = self.allKashi.length - self.phrases.length;
+            // console.log(self.phrases);
+          }).fail(function(){
+            console.log('fail to road data _r');
+          });
+          $.get(create_url_kana(self.$route.params.song_id), function(data){
+            self.allGuid = data;
+            self.guidPhrases = self.allGuid.split(/\r\n|\r|\n/);
+            self.guidPhrases.pop();
+            // console.log(self.guidPhrases);
+          }).fail(function(){
+            console.log('fail to road data _k');
+          });
         },
-        created: function () {
-          this.loadAllKashi();
+        gameStart: function(){
+            this.stillStart = false;
+            this.isPlaying = true;
         },
-        computed: {
-          inputStringCurrent: function(index){
-            return this.inputString;
-          },
-          getCombo: function(){
-            return this.combo;
-          },
-          getMaxCombo: function(){
-            return this.maxCombo;
+        innerRange: function(index){
+          return (index>=this.onfocus && index < this.onfocus+3) ? true: false;
+        },
+        checkLastInput: function(index){
+          if( this.inputChar ==  this.phrases[ index ][ this.inputString.length ] ){//入力正誤判定
+            this.inputString = this.inputString + this.inputChar;
+            this.failInput = '';
+            this.combo++;
           }
-        },
-        methods: {
-          loadAllKashi: function() {
-            var self = this;
-            $.get(create_url_roma(self.$route.params.song_id), function(data){
-              self.allKashi = data;
-              self.phrases = self.allKashi.split(/\r\n|\r|\n/);
-              self.phrases.pop();
-              self.phrasesLength = self.phrases.length;
-            }).fail(function(){
-              console.log('fail_roma');
-            });
-            $.get(create_url_kana(self.$route.params.song_id), function(data){
-              self.allGuid = data;
-              self.guidPhrases = self.allGuid.split(/\r\n|\r|\n/);
-              self.guidPhrases.pop();
+          else {
+            this.failInput = this.inputChar;
+            if(this.combo > this.maxCombo)
+              this.maxCombo = this.combo;
+            this.combo = 0;
+          }
+          this.inputChar = '';
 
-              //console.log(phrases);
-            }).fail(function(){
-              console.log('fail_kana');
-            });
-          },
-          gameStart: function(){
-              this.stillStart = false;
-              this.isPlaying = true;
-          },
-          innerRange: function(index){
-            return (index>=this.onfocus && index < this.onfocus+3) ? true: false;
-          },
-          checkLastInput: function(index){
-            if( this.inputChar ==  this.phrases[ index ][ this.inputString.length ] ){//入力正誤判定
-              this.inputString = this.inputString + this.inputChar;
-              this.failInput = '';
-              this.combo++;
-            }
-            else {
-              this.failInput = this.inputChar;
-              if(this.combo > this.maxCombo)
-                this.maxCombo = this.combo;
-              this.combo = 0;
-            }
-            this.inputChar = '';
-
-            if( this.inputString == this.phrases[ index ] ){//行終了判定
-              this.onfocus++;
-              this.inputString = '';
-            }
-            if( this.onfocus == this.phrasesLength ){//クリア判定
-              this.isPlaying = false;
-              this.isClear = true;
-            }
+          if( this.inputString == this.phrases[ index ] ){//行終了判定
+            this.onfocus++;
+            this.inputString = '';
+          }
+          if( this.onfocus == this.phrasesLength ){//クリア判定
+            if(this.combo > this.maxCombo) this.maxCombo = this.combo;
+            this.combo = 0;
+            this.isPlaying = false;
+            this.isClear = true;
           }
         }
-      })
-    },
-    '/setRanking': {
-      component: Vue.extend({
-        template: '#clear'
-      })
-    }
+      }
+    })
+  },
+  '/setRanking': {
+    component: Vue.extend({
+      template: '#clear'
+    })
+  }
 });
 
 router.start(App, '#app')
